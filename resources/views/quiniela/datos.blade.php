ceb8ea1e5ad065c111a2dc645e0304c829429464
creativef25 <creativef25@gmail.com>
2019-11-06 00:15:34 -0600

    formulario con direccion

--- resources/views/quiniela/datos.blade.php
+++ resources/views/quiniela/datos.blade.php
@@ -24,65 +24,75 @@
               <form class="rd-form">
                 <div class="form-wrap">
                   <div class="row row-10 row-narrow">
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                       <div class="form-wrap">
-                        <label class="form-label" for="biling-first-name">First Name</label>
-                        <input class="form-input" id="biling-first-name" type="text" name="name" data-constraints="@Required">
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-wrap">
-                        <label class="form-label" for="biling-family-name">Family Name</label>
-                        <input class="form-input" id="biling-family-name" type="text" name="name" data-constraints="@Required">
+                        <label class="form-label" for="biling-first-name">Nombre</label>
+                        <input class="form-input" id="biling-first-name" type="text" name="nombre" data-constraints="@Required">
                       </div>
                     </div>
                     <div class="col-md-12">
                       <div class="form-wrap">
-                        <label class="form-label" for="biling-company">Company</label>
-                        <input class="form-input" id="biling-company" type="text" name="name" data-constraints="@Required">
+                        <label class="form-label" for="biling-family-name">Apellido Paterno</label>
+                        <input class="form-input" id="biling-family-name" type="text" name="apePaterno" data-constraints="@Required">
                       </div>
                     </div>
                     <div class="col-md-12">
                       <div class="form-wrap">
-                        <label class="form-label" for="biling-address">Address</label>
-                        <input class="form-input" id="biling-address" type="text" name="name" data-constraints="@Required">
+                        <label class="form-label" for="biling-family-name">Apellido Materno</label>
+                        <input class="form-input" id="biling-family-name" type="text" name="apeMaterno" data-constraints="@Required">
                       </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                       <div class="form-wrap">
-                        <label class="form-label" for="biling-country">Country</label>
-                        <input class="form-input" id="biling-country" type="text" name="name" data-constraints="@Required">
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-wrap">
-                        <label class="form-label" for="biling-city">City/Town</label>
-                        <input class="form-input" id="biling-city" type="text" name="name" data-constraints="@Required">
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-wrap">
-                        <label class="form-label" for="biling-apartment">Apartment/Suite No.</label>
-                        <input class="form-input" id="biling-apartment" type="text" name="name" data-constraints="@Required">
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-wrap">
-                        <label class="form-label" for="biling-phone">Phone</label>
-                        <input class="form-input" id="biling-phone" type="text" name="phone" data-constraints="@Required @Numeric">
+                        <label class="form-label" for="biling-company">Edad</label>
+                        <input class="form-input" id="biling-company" type="text" name="edad" data-constraints="@Required">
                       </div>
                     </div>
                   </div>
                 </div>
-              </form>
             </div>
-
+            <div class="col-lg-6">
+              <article class="heading-component">
+                <div class="heading-component-inner">
+                  <h5 class="heading-component-title">Ingresa tu Direccion</h5>
+                </div>
+              </article>
+              <div class="col-md-12">
+                <div class="form-wrap">
+                  <label class="form-label" for="biling-address">Calle</label>
+                  <input class="form-input" id="biling-address" type="text" name="calle" data-constraints="@Required">
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="form-wrap">
+                  <label class="form-label" for="biling-country">Alcaldia</label>
+                  <input class="form-input" id="biling-country" type="text" name="alcaldia" data-constraints="@Required">
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="form-wrap">
+                  <label class="form-label" for="biling-city">Ciudad</label>
+                  <input class="form-input" id="biling-city" type="text" name="ciudad" data-constraints="@Required">
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="form-wrap">
+                  <label class="form-label" for="biling-apartment">Codigo Postal.</label>
+                  <input class="form-input" id="biling-apartment" type="text" name="cp" data-constraints="@Required">
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="form-wrap">
+                  <label class="form-label" for="biling-phone">Telefono</label>
+                  <input class="form-input" id="biling-phone" type="text" name="telefono" data-constraints="@Required @Numeric">
+                </div>
+              </div>
+            </div>
+            <button type="submit" name="button" class="btn btn-primary">Guardar</button>
+          </form>
           </div>
-
         </div>
-
       </div>
-
     </div>
 
   </section>
